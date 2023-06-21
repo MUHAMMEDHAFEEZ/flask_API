7956351847c5a54b122548cad6516c89f13229f7
MUHAMMEDHAFEEZ <621172@oi.edu.eg>
2023-06-21 13:39:55 +0300

    test api

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from flask import Flask, request,jsonify
 
 app = Flask(__name__)
 
-@app.route("/get_user/<user_id>")
+@app.route("/get-user/<user_id>")
 def get_user(user_id):
     user_data = {
         "user_id" : user_id,
         "name": "mohammed hafeez",
-        "email": "john.doe@example.com",
+        "email": "mohammedhafeez@example.com",
     }
     
     extra = request.args.get("extra")
@@ -15,5 +15,12 @@ def get_user(user_id):
         user_data["extra"] = extra
     
     return jsonify(user_data) , 200 
+
+@app.route("/create-user" , methods=["POST"])
+def create_user():
+    data = request.get_json()
+    
+    return jsonify(data) , 201
+
 if __name__ == "__main__":
-     app.run(debug=True)
+      app.run(debug=True)
